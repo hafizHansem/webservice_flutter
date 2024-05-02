7fad1cbb58bfff192872ccfb73921a14954eca10
hafizHansem <hardcore.soldier25@gmail.com>
2024-05-02 22:06:12 +0700

    tugas praktikum pemmob

--- lib/main.dart
+++ lib/main.dart
@@ -2,112 +2,84 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
-
-class PopulasiTahun {
-  int tahun;
-  int populasi;
-  PopulasiTahun({required this.tahun, required this.populasi});
-}
-
-class Populasi {
-  List<PopulasiTahun> ListPop = <PopulasiTahun>[];
-
-  Populasi(Map<String, dynamic> json) {
-    // isi listPop disini
-    var data = json["data"];
-    for (var val in data) {
-      var tahun = int.parse(val["Year"]); //thn dijadikan int
-      var populasi = val["Population"]; //pouliasi sudah int
-      ListPop.add(PopulasiTahun(tahun: tahun, populasi: populasi));
-    }
-  }
-  //map dari json ke atribut
-  factory Populasi.fromJson(Map<String, dynamic> json) {
-    return Populasi(json);
-  }
-}
-
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() {
-    return MyAppState();
+// Model data untuk universitas
+class University {
+  final String name;
+  final String website;
+
+  University({required this.name, required this.website});
+
+  // Factory method untuk membuat objek University dari JSON
+  factory University.fromJson(Map<String, dynamic> json) {
+    return University(
+      name: json['name'],
+      website: json['web_pages'][0],
+    );
   }
 }
 
-//class state
-class MyAppState extends State<MyApp> {
-  late Future<Populasi> futurePopulasi;
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
 
-  //https://datausa.io/api/data?drilldowns=Nation&measures=Population
-  String url =
-      "https://datausa.io/api/data?drilldowns=Nation&measures=Population";
-
-  //fetch data
-  Future<Populasi> fetchData() async {
-    final response = await http.get(Uri.parse(url));
-
-    if (response.statusCode == 200) {
-      // jika server mengembalikan 200 OK (berhasil),
-      // parse json
-      return Populasi.fromJson(jsonDecode(response.body));
-    } else {
-      // jika gagal (bukan  200 OK),
-      // lempar exception
-      throw Exception('Gagal load');
-    }
-  }
+class _MyAppState extends State<MyApp> {
+  late Future<List<University>> futureUniversities;
 
   @override
   void initState() {
     super.initState();
-    futurePopulasi = fetchData();
+    futureUniversities = fetchUniversities();
+  }
+
+  // Method untuk mengambil data universitas dari API
+  Future<List<University>> fetchUniversities() async {
+    final response = await http.get(Uri.parse(
+        'http://universities.hipolabs.com/search?country=Indonesia'));
+    if (response.statusCode == 200) {
+      List<dynamic> data = jsonDecode(response.body);
+      List<University> universities = data
+          .map((university) => University.fromJson(university))
+          .toList();
+      return universities;
+    } else {
+      throw Exception('Failed to load universities');
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'coba http',
-        home: Scaffold(
-          appBar: AppBar(
-            title: const Text('coba http'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Indonesian Universities'),
+        ),
+        body: Center(
+          child: FutureBuilder<List<University>>(
+            future: futureUniversities,
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                return ListView.builder(
+                  itemCount: snapshot.data!.length,
+                  itemBuilder: (context, index) {
+                    return ListTile(
+                      title: Text(snapshot.data![index].name),
+                      subtitle: Text(snapshot.data![index].website),
+                    );
+                  },
+                );
+              } else if (snapshot.hasError) {
+                return Text('${snapshot.error}');
+              }
+              return CircularProgressIndicator();
+            },
           ),
-          body: Center(
-            child: FutureBuilder<Populasi>(
-              future: futurePopulasi,
-              builder: (context, snapshot) {
-                if (snapshot.hasData) {
-                  return Center(
-                    //gunakan listview builder
-                    child: ListView.builder(
-                      itemCount: snapshot
-                          .data!.ListPop.length, //asumsikan data ada isi
-                      itemBuilder: (context, index) {
-                        return Container(
-                            decoration: BoxDecoration(border: Border.all()),
-                            padding: const EdgeInsets.all(14),
-                            child: Column(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  Text(snapshot.data!.ListPop[index].tahun
-                                      .toString()),
-                                  Text(snapshot.data!.ListPop[index].populasi
-                                      .toString()),
-                                ]));
-                      },
-                    ),
-                  );
-                } else if (snapshot.hasError) {
-                  return Text('${snapshot.error}');
-                }
-                // By default, show a loading spinner.
-                return const CircularProgressIndicator();
-              },
-            ),
-          ),
-        ));
+        ),
+      ),
+    );
   }
 }
